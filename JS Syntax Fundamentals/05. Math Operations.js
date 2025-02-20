c0efc57948be552d623731d27de716459fe41181
mariokoshn@gmail.com <144950133+Mario8802@users.noreply.github.com>
2025-02-20 09:45:38 +0100

    Adding more solutions

---
+++ JS Syntax Fundamentals/05. Math Operations.js
@@ -0,0 +1,39 @@
+// function mathOperations(number1, number2, operator){
+//     if (operator == '+') {
+//         console.log(number1 + number2)
+//      } 
+//     else if (operator == '-') {
+//         console.log(number1 - number2) 
+//     }
+
+//     else if (operator == '*') {
+//         console.log(number1 * number2)
+//     }
+
+//     else if (operator == '/') {
+//         console.log(number1 / number2)
+//     }
+
+//     else if (operator == '%') {
+//         console.log(number1 % number2)
+//     }
+
+//     else if (operator == '**') {
+//         console.log(number1 ** number2)
+//     }
+// }
+
+
+function solve(num1, num2, operator) {
+
+    let result;
+    switch (operator) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/': result = num1 / num2; break;
+        case '%': result = num1 % num2; break;
+        case '**': result = num1 ** num2; break;
+    }
+    console.log(result);
+}

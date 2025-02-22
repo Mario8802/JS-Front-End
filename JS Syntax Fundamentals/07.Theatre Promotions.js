77af48bb90d39c3cc0b35a21956c8d863624d094
mariokoshn@gmail.com <144950133+Mario8802@users.noreply.github.com>
2025-02-22 12:23:55 +0100

    Added more solutions

---
+++ JS Syntax Fundamentals/07.Theatre Promotions.js
@@ -0,0 +1,39 @@
+function ticketSale(day, age) {
+    if (age < 0 || age > 122) {
+        console.log("Error!");
+        return;
+    }
+
+    let ticketPrice = 0;
+
+    if (day === 'Weekday') {
+        if (age <= 18) {
+            ticketPrice = 12;
+        } else if (age <= 64) {
+            ticketPrice = 18;
+        } else {
+            ticketPrice = 12;
+        }
+    } else if (day === 'Weekend') {
+        if (age <= 18) {
+            ticketPrice = 15;
+        } else if (age <= 64) {
+            ticketPrice = 20;
+        } else {
+            ticketPrice = 15;
+        }
+    } else if (day === 'Holiday') {
+        if (age <= 18) {
+            ticketPrice = 5;
+        } else if (age <= 64) {
+            ticketPrice = 12;
+        } else {
+            ticketPrice = 10;
+        }
+    } else {
+        console.log("Error!");
+        return;
+    }
+
+    console.log(`${ticketPrice}$`);
+}

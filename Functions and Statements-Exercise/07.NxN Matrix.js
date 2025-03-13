bbf419b0211f37e3616472019934f57530b21064
mariokoshn@gmail.com <144950133+Mario8802@users.noreply.github.com>
2025-03-13 15:18:30 +0100

    Added more solutions

---
+++ Functions and Statements-Exercise/07.NxN Matrix.js
@@ -0,0 +1,21 @@
+// function solve(num) {
+//     for (let i = 0; i < num; i++) {
+//         let row = '';
+//         for (let j = 0; j < num; j++) {
+//             row += num + ' ';
+//         }
+//         console.log(row.trim());
+//     }
+// }
+
+
+function solve(num) {
+    let row = (num + '').repeat(num).trim();
+    for (let i = 0; i < num; i++) {
+        console.log(row)
+    } 
+}
+
+
+
+solve(3);

7e6fbb280012a96b6211968912c8b0d71a149fd1
mariokoshn@gmail.com <144950133+Mario8802@users.noreply.github.com>
2025-04-07 23:55:49 +0200

    Adding more solutions

--- DOM Introduction - Lab/05.Colorize Table/colorize-table.js
+++ DOM Introduction - Lab/05.Colorize Table/colorize-table.js
@@ -1,3 +1,34 @@
 function colorize() {
-    //TODO
+    const tableElement = document.querySelector('table tr');
+    const tableRowElements = tableElement.children;
+
+    for (let i = 0; i < tableRowElements.length; i+=2) {
+        tableRowElements[i].style.backgroundColor = 'teal';
+    }
+}
+
+function colorize() {
+    const tableElement = document.querySelector('table tbody');
+    const tableRowElements = tableElement.children;
+
+    for (let i = 0; i < tableRowElements.length; i+=2) {
+        tableRowElements[i].style.backgroundColor = 'yellow';
+    }
+}
+
+function colorize() {
+    const rows = document.querySelectorAll('table tr');
+
+    for (let i = 1; i < rows.length; i += 2) {
+        rows[i].style.backgroundColor = 'teal';
+    }
+}
+
+
+function colorize() {
+    const rowsToBeColored = document.querySelerctorAll('h1');
+
+    for (let i = 0; i < rowsToBeColored.length; i++) {
+        rowsToBeColored[i].style.backgroundColor = 'teal';
+    }
 }
--- DOM Introduction - Lab/07.Extract Parenthesis/extract-parenthesis.js
+++ DOM Introduction - Lab/07.Extract Parenthesis/extract-parenthesis.js
@@ -1,3 +1,9 @@
 function extract(content) {
-    // TODO
+    const contentElement = document.getElementById('content');
+
+    const pattern = /\(([^()]+)\)/g;
+    const matches = contentElement.textContent.matchAll(pattern);
+    const result = Array.from(matches).map(match => match[1]).join(';');
+    return result;
+
 }

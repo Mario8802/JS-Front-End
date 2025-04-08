dbb6dc2d2c085f3489656cd000feeea84335d2ac
mariokoshn@gmail.com <144950133+Mario8802@users.noreply.github.com>
2025-04-09 00:58:49 +0200

    Adding more solutions

---
+++ DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
@@ -0,0 +1,14 @@
+function addItem() {
+    const itemsElement = document.getElementById('items');
+    const inputElement = document.getElementById('newItemText');
+
+    const itemText = inputElement.value;
+
+    const liElement = document.createElement('li');
+
+    liElement.textContent = itemText;
+
+    itemsElement.appendChild(liElement);
+
+    inputElement.value = '';
+}

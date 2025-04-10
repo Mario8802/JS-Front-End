b6d63606f3f27d5e19c3ab6e2a53b9b85e9ddb18
mariokoshn@gmail.com <144950133+Mario8802@users.noreply.github.com>
2025-04-10 16:48:27 +0200

    Adding more solutions

--- DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,33 @@
 function addItem() {
-    //TODO...
+    // Get element references
+    const itemsElement = document.getElementById('items');
+    const newItemTextElement = document.getElementById('newItemText');
+    
+    // Create new element with textContent of the input field
+    const liElement = document.createElement('li');
+    liElement.textContent = newItemTextElement.value;
+
+    // Create delete button
+    const deleteButton = document.createElement('a');
+    deleteButton.textContent = '[Delete]';
+    
+    // deleteButton.setAttribute('href', '#');
+    deleteButton.href = '#';
+    
+    // Attach event to delete button 
+    deleteButton.addEventListener('click', (ev) => {
+        ev.currentTarget.parentElement.remove();
+    })
+
+    // Add button to liElement
+    liElement.append(deleteButton);
+
+    // Attach liElement to Dom
+    itemsElement.appendChild(liElement);
+
+    // Clear input field
+    newItemTextElement.value = '';
+
+    
+
 }

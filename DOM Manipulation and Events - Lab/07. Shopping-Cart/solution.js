f41fe6a9af0c3796a51d9763e8dcf31add40af02
mariokoshn@gmail.com <144950133+Mario8802@users.noreply.github.com>
2025-04-11 09:03:48 +0200

    Adding more solutions

--- DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
+++ DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,44 @@
 function solve() {
-   //TODO...
+   //
+   // Get element references
+   const resultElement = document.querySelector(textarea['disabled']);
+   const checkoutButton = document.querySelector('button.checkout');
+   const productCatalogElement = document.querySelectorAll('.shopping-cart');
+
+   let products = [];
+
+   //
+   // Attach event for add product
+   productCatalogElement.addEventListener('click', (e) => {
+      //Event delegation 
+      if (e.target.tagName === 'BUTTON') {
+         return;
+   }
+
+   // Get product name and price
+   const productElement = e.target.closest('.product');
+   const name = productElement.querySelector('.product-title').textContent;
+   const price = Number(productElement.querySelector('.product-line-price').textContent);
+
+   // Printing text area
+   resultElement.value += `Added ${name} for ${price.toFixed(2)} to the cart.\n`;
+});
+
+
+   products.push({
+      name,
+      price
+   });
+});
+   // Attach checkout event
+   checkElement.value = `Added ${name} for ${price.toFixed(2)} to the cart.\n`;
+   const totalprice = products.reduce((price, product) => price + product.price, 0);
+
+   const productList = product.map(product => product.name).join(', ');
+
+   resulktElement += `You bought ${productList} for ${totalPrice.toFixed(2)}.\n`;
+
+   document.querySelector(GamepadButton.add-product)
+      .foreach(e => e.setAttribute(disabled, disabled))
+
 }

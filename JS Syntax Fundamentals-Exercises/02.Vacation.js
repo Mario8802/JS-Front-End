dec2140ea1d5f9568fc0ff6ed3d424f569ddff27
mariokoshn@gmail.com <144950133+Mario8802@users.noreply.github.com>
2025-04-18 00:43:22 +0200

    Adding more solutions

--- JS Syntax Fundamentals-Exercises/02.Vacation.js
+++ JS Syntax Fundamentals-Exercises/02.Vacation.js
@@ -37,3 +37,58 @@ function vacationCosts(count, type, day) {
 // vacationCosts(110, "Business", "Friday");
 // vacationCosts(15, "Regular", "Saturday");
 // vacationCosts(5, "Students", "Friday");
+
+
+// You are given a group of people, the 
+// type of the group, and the day of the week they 
+// are going to stay. Based on that information calculate 
+// how much they have to pay and print that price on the
+//  console. Use the table below. In each cell is the price for a single person.
+
+// The output should look like that: `Total price: {price}`.
+//  The price should be formatted to the second decimal point.
+
+// Friday Saturday
+
+function solve(count, people) {
+    let price = 0
+    if (people == 'Students') {
+        if (day == 'Friday') {
+            price = count * 8.45
+        } else if (day == 'Saturday') {
+            price = count * 9.80
+        } else if (day == 'Sunday') {
+            price = count * 10.46
+        }
+
+    } else if (people == 'Business') {
+        if (day == 'Friday') {
+            price = count * 10.90
+        } else if (day == 'Saturday') {
+            price = count * 15.60
+        } else if (day == 'Sunday') {
+            price = count * 16
+        }
+
+    } else if (people == 'Regular') {
+        if (day == 'Friday') {
+            price = count * 15
+        } else if (day == 'Saturday') {
+            price = count * 20
+        } else if (day == 'Sunday') {
+            price = count * 22.50
+        } 
+
+    }
+
+    if (people == 'Students' && count >= 30) {
+        price = price * 0.85
+    }
+    if (people == 'Business' && count >= 100) {
+        price = price - (10 * price / count)
+    }
+    if (people == 'Regular' && count >= 10 && count <= 20) {
+        price = price * 0.95
+    }
+    console.log(`Total price: {price.toFixed(2)}`)
+}
--- JS Syntax Fundamentals-Exercises/03.Leap Year.js
+++ JS Syntax Fundamentals-Exercises/03.Leap Year.js
@@ -1,7 +1,30 @@
-function leapYear(year) {
-    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
-        console.log("yes");
-    } else {
-        console.log("no");
-    }
+// function leapYear(year) {
+//     if (year % 4 ==0 && year % 100 != 0 || year % 400 == 0){
+//         return "yes"
+//     } 
+
+//     return "no"
+    
+// }
+
+
+// function printingNumbers(start, end) {
+//     let numbers = ''
+//     let sumNums = 0
+//     for (let i = start; i <= end; i++) {
+//         numbers += i + ' '
+//         sumNums += i
+//     }
+//     console.log(numbers)
+//     console.log(`Sum: ${sumNums}`)
+// }
+
+// printingNumbers(5, 10) 
+
+
+function solve(first, second , third) {
+    return `${third} ${second} ${first}`
+    
 }
+
+console.log(solve('A', 'B', 'C')) // C B A

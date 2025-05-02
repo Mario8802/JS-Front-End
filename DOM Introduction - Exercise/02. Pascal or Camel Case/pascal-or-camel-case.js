8a2a6f08f3332639f53ab444b44f0649f296b9d0
mariokoshn@gmail.com <144950133+Mario8802@users.noreply.github.com>
2025-05-02 22:23:20 +0200

    Adding more solutions

--- DOM Introduction - Exercise/02. Pascal or Camel Case/pascal-or-camel-case.js
+++ DOM Introduction - Exercise/02. Pascal or Camel Case/pascal-or-camel-case.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO
+  const text = document.getElementById('text').value;
+  const convention = document.getElementById('naming-convention').value;
+  const resultElement = document.getElementById('result');
+
+  const words = text.toLowerCase().split(' ');
+
+  let result = '';
+
+  if (convention === 'Camel Case') {
+      result = words[0];
+      for (let i = 1; i < words.length; i++) {
+          result += words[i][0].toUpperCase() + words[i].slice(1);
+      }
+  } else if (convention === 'Pascal Case') {
+      for (let word of words) {
+          result += word[0].toUpperCase() + word.slice(1);
+      }
+  } else {
+      result = 'Error!';
+  }
+
+  resultElement.textContent = result;
 }

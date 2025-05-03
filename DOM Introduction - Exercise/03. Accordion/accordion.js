79f1144d0454a2d38d9db78eb301f7f9105931c9
mariokoshn@gmail.com <144950133+Mario8802@users.noreply.github.com>
2025-05-03 08:18:24 +0200

    Adding more solutions

--- DOM Introduction - Exercise/03. Accordion/accordion.js
+++ DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,15 @@
 function toggle() {
-    //TODO
+    const extraInformationElement = document.getElementById('extra');
+    const toggleButtonElement = document.querySelector('.head span.button');
+
+    const currentButtonText = toggleButtonElement.textContent;
+    if (currentButtonText === 'More') {
+        extraInformationElement.style.display = 'block';
+        toggleButtonElement.textContent = 'Less';
+    } else {
+        extraInformationElement.style.display = 'none';
+        toggleButtonElement.textContent = 'More';
+
+    }
+
 }

2d08e9f96cb7cccecbba55d2506965d8751cd9d4
mariokoshn@gmail.com <144950133+Mario8802@users.noreply.github.com>
2025-05-03 22:32:05 +0200

    Adding more solutions

--- DOM Introduction - Exercise/05. Table - Search Engine/table-search-engine.js
+++ DOM Introduction - Exercise/05. Table - Search Engine/table-search-engine.js
@@ -1,3 +1,19 @@
 function solve() {
-   //TODO
+   const input = document.getElementById('searchField');
+   const searchText = input.value.toLowerCase();
+   const rows = Array.from(document.querySelectorAll('table.container tbody tr'));
+
+   for (let row of rows) {
+       row.classList.remove('select'); 
+       const cells = Array.from(row.children);
+
+       for (let cell of cells) {
+           if (cell.textContent.toLowerCase().includes(searchText)) {
+               row.classList.add('select');
+               break; 
+           }
+       }
+   }
+
+   input.value = ''; 
 }

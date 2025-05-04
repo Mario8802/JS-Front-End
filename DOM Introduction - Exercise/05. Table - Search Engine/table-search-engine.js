b79985c1fc1aa1bfd166d7c26c5826de0855da2e
mariokoshn@gmail.com <144950133+Mario8802@users.noreply.github.com>
2025-05-04 20:58:25 +0200

    Add more solutions

--- DOM Introduction - Exercise/05. Table - Search Engine/table-search-engine.js
+++ DOM Introduction - Exercise/05. Table - Search Engine/table-search-engine.js
@@ -1,19 +1,71 @@
 function solve() {
-   const input = document.getElementById('searchField');
-   const searchText = input.value.toLowerCase();
-   const rows = Array.from(document.querySelectorAll('table.container tbody tr'));
+   let rows = document.querySelectorAll('.container tbody tr');
+   let input = document.getElementById('searchField');
 
-   for (let row of rows) {
-       row.classList.remove('select'); 
-       const cells = Array.from(row.children);
+   let pattern = input.value;
 
-       for (let cell of cells) {
-           if (cell.textContent.toLowerCase().includes(searchText)) {
-               row.classList.add('select');
-               break; 
-           }
-       }
+   if(!pattern) {
+      return;
    }
 
-   input.value = ''; 
+   for (const row of rows) {
+      if (row.textContent.includes(pattern)) {
+         row.classList.add('select');
+      } else {
+         row.classList.remove('select');
+      }
+   }
+
+   input.value = '';
 }
+
+
+// function solve() {
+//    const input = document.getElementById('searchField');
+//    const searchText = input.value.toLowerCase();
+//    const rows = Array.from(document.querySelectorAll('table.container tbody tr'));
+
+//    for (let row of rows) {
+//        row.classList.remove('select'); 
+//        const cells = Array.from(row.children);
+
+//        for (let cell of cells) {
+//            if (cell.textContent.toLowerCase().includes(searchText)) {
+//                row.classList.add('select');
+//                break; 
+//            }
+//        }
+//    }
+
+//    input.value = ''; 
+// }
+
+
+// function solve() {
+//    document.querySelector('#searchBtn').addEventListener('click', onclick);
+   
+//    function onclick() {
+//       const trElements = document.querySelectorAll('table.container tbody tr');
+//       const searchFieldElement = document.getElementById('searchField')
+//       const searchText = searchFieldElement.value;
+
+//       for (const trElement of trElements) {
+//          const tdElements = trElement.querySelectorAll('td');
+//          let isSelected = false;
+
+//          trElement.className.remove('select');
+
+//          for (const tdElement of tdElements) {
+//             if (tdElement.textContent.includes(searchText)) {
+//                isSelected = true;
+//                break;
+//             }  
+//          }
+//          if (isSelected) {
+//             trElement.className.add('select');
+//          }
+//       }
+
+//       searchFieldElement.value = '';
+//    }
+// }

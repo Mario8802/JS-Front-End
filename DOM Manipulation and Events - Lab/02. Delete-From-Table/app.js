cf185c8a4196b6d41bb58cfc0bcdbd99aab426f7
mariokoshn@gmail.com <144950133+Mario8802@users.noreply.github.com>
2025-05-07 21:55:16 +0200

    Added more solutions

--- DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
+++ DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
@@ -1,18 +1,42 @@
+// function deleteByEmail() {
+//     const customerTableElement = document.getElementById('customers');
+//     const inputElement = document.querySelector('input[type=text][name="email"]');
+//     const resultElement = document.getElementById('result');
+
+//     const searchEmail = inputElement.value;
+
+//     const tdElements = customerTableElement.querySelectorAll('tbody td:last-child');
+
+//     const searchElement = Array.from(tdElements).find(td => td.textContent === searchEmail);
+
+//     if (searchElement) {
+//         searchElement.parentElement.remove();
+//         resultElement.textContent = 'Deleted.';
+//     } else {
+//         resultElement.textContent = 'Not found.';
+//     }
+// }   
+
 function deleteByEmail() {
-    const customerTableElement = document.getElementById('customers');
-    const inputElement = document.querySelector('input[type=text][name="email"]');
+    const inputEmailElement = document.querySelector('input[name=email]');
     const resultElement = document.getElementById('result');
+    const trElements = document.querySelectorAll('table#customers tbody tr');
 
-    const searchEmail = inputElement.value;
 
-    const tdElements = customerTableElement.querySelectorAll('tbody td:last-child');
+    //Search for tr element
+    const resultTrElement = Array
+        .from(trElements)
+        .find(trElement => trElement.children[1].textContent.includes(inputEmailElement.value))
+    if (resultTrElement) {
+        //Delete table row
+        resultTrElement.remove();
 
-    const searchElement = Array.from(tdElements).find(td => td.textContent === searchEmail);
-
-    if (searchElement) {
-        searchElement.parentElement.remove();
+        //SHow deleted text
         resultElement.textContent = 'Deleted.';
     } else {
         resultElement.textContent = 'Not found.';
     }
-}   
+    //Clear input
+    inputEmailElement.value = '';
+
+}

d5ce1d5ab256fad6b4f3e8c84340978cb9a1e8da
mariokoshn@gmail.com <144950133+Mario8802@users.noreply.github.com>
2025-05-08 12:45:50 +0200

    Added more solutions

--- DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
+++ DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
@@ -13,3 +13,18 @@ function attachGradientEvents() {
         resultElement.textContent = `${percentage}%`;
     });
 }
+
+
+const button = document.getElementsByTagName('div'[0]);
+button.addEventListener('mouseover', function (e) {
+    const style = e.currentTarget;
+    const { backgroundColor } = style;
+
+    if (backgroundColor === 'white') {
+        targetStyles.backgroundColor = '234465';
+        targetStyles.color = 'white';
+    } else {
+        targetStyles.backgroundColor = 'white';
+        targetStyles.color = '234465';
+    }
+})

8f251992258fd4f667a9166a790b1ebe862aa56b
mariokoshn@gmail.com <144950133+Mario8802@users.noreply.github.com>
2025-05-11 21:23:25 +0200

    Added more solutions

--- DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
+++ DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
@@ -1,5 +1,39 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const encodeTextarea = document.querySelector('#encode textarea');
+    const decodeTextarea = document.querySelector('#decode textarea');
+
+    const encodeBtn = document.querySelector('#encode button');
+    const decodeBtn = document.querySelector('#decode button');
+
+    encodeBtn.addEventListener('click', encodeMessage);
+    decodeBtn.addEventListener('click', decodeMessage);
+
+    function encodeMessage(e) {
+        e.preventDefault(); 
+
+        const input = encodeTextarea.value;
+        let encoded = '';
+
+        for (let i = 0; i < input.length; i++) {
+            encoded += String.fromCharCode(input.charCodeAt(i) + 1);
+        }
+
+        decodeTextarea.value = encoded;
+        encodeTextarea.value = '';
+    }
+
+    function decodeMessage(e) {
+        e.preventDefault();
+
+        const encoded = decodeTextarea.value;
+        let decoded = '';
+
+        for (let i = 0; i < encoded.length; i++) {
+            decoded += String.fromCharCode(encoded.charCodeAt(i) - 1);
+        }
+
+        decodeTextarea.value = decoded;
+    }
 }
